7c890bcecc18d336c478ad455b1873b016012aea
Yannick Lohse <contact@yannick-lohse.fr>
2019-12-19 10:27:02 +0100

    fix: Import path

--- src/components/notes/List/AppTitle.jsx
+++ src/components/notes/List/AppTitle.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { MainTitle } from 'cozy-ui/react/Text'
 import { translate } from 'cozy-ui/react/I18n'
-import { withBreakpoints } from 'cozy-ui/transpiled/react'
+import { withBreakpoints } from 'cozy-ui/react'
 
 //We don't not display the Title this way in Mobile.
 //We use Bar.centrer
