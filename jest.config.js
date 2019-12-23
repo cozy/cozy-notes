f5ee7d3ef21d56e8e2999705c6ba9e53e650e5f3
Quentin Valmori <quentin.valmori@gmail.com>
2019-12-23 11:07:18 +0100

    tests: Configure jest to handle files

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,15 @@ module.exports = {
   moduleNameMapper: {
     '\\.(png|gif|jpe?g|svg)$': '<rootDir>/test/__mocks__/fileMock.js',
     // identity-obj-proxy module is installed by cozy-scripts
-    '\\.(css|styl)$': 'identity-obj-proxy'
+    '\\.(css|styl)$': 'identity-obj-proxy',
+    '^cozy-client$': 'cozy-client/dist/index.js',
+    'cozy-ui/react': 'cozy-ui/transpiled/react'
   },
   transformIgnorePatterns: ['node_modules/(?!cozy-ui)'],
+  transform: {
+    '^.+\\.webapp$': '<rootDir>/test/jestLib/json-transformer.js',
+    '^.+\\.(js|jsx)?$': '<rootDir>/test/jestLib/babel-transformer.js'
+  },
   globals: {
     __ALLOW_HTTP__: false,
     __TARGET__: 'browser',
