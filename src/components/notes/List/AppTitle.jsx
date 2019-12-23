85abb4f3c62a57daaa7b34c397240c5443e2904e
Quentin Valmori <quentin.valmori@gmail.com>
2019-12-23 09:44:15 +0100

    refactor: Use named import for style

--- src/components/notes/List/AppTitle.jsx
+++ src/components/notes/List/AppTitle.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { MainTitle } from 'cozy-ui/react/Text'
 import { translate } from 'cozy-ui/react/I18n'
 import { withBreakpoints } from 'cozy-ui/react'
+import styles from 'components/notes/List/list.styl'
 
 //We don't not display the Title this way in Mobile.
 //We use Bar.centrer
 const AppTitle = ({ t, breakpoints: { isMobile } }) => {
   return isMobile ? null : (
-    <MainTitle className="titlePadding">{t('Notes.List.my_notes')}</MainTitle>
+    <MainTitle className={styles.titlePadding}>
+      {t('Notes.List.my_notes')}
+    </MainTitle>
   )
 }
 
