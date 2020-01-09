4e6f6c5a2ab6655acddbb376f849ed3c95e167a0
Éric D <15271+edas@users.noreply.github.com>
2020-01-09 11:51:21 +0100

    feat: Sharing button with label on large view

--- src/components/notes/sharing.jsx
+++ src/components/notes/sharing.jsx
@@ -1,70 +1,32 @@
-import React, { useCallback, useState, useMemo, useEffect } from 'react'
+import React, { useCallback, useState } from 'react'
 import SharingProvider, { ShareButton, ShareModal } from 'cozy-sharing'
-import { withClient, models } from 'cozy-client'
 import { withLocales } from 'cozy-sharing'
+import { withClient } from 'cozy-client'
+
+import useFileWithPath from 'hooks/useFileWithPath'
+import styles from 'components/notes/sharing.styl'
 
 const LocalizedSharingProvider = withLocales(SharingProvider)
 
-function normalizeFileAndPath(id, file, parent) {
-  const withId = { id, ...file }
-  const normalized = models.file.normalize(withId)
-  if (parent && parent.path) {
-    const withPath = models.file.ensureFilePath(normalized, parent)
-    return withPath
-  } else {
-    return normalized
-  }
-}
-
 export default withClient(function SharingWidget(props) {
   const { client } = props
-  const [parent, setParent] = useState(undefined)
 
-  const id = props.file.id || props.file._id
-
-  const file = useMemo(() => normalizeFileAndPath(id, props.file, parent), [
-    id,
-    parent
-  ])
-
-  useEffect(
-    () => {
-      async function getParent(file) {
-        try {
-          const parent = await client.query(
-            client.get('io.cozy.files', file.attributes.dir_id)
-          )
-          setParent(parent.data)
-        } catch (e) {
-          // eslint-disable-next-line no-console
-          console.warn(
-            `Request failed when try to fetch the parent ${
-              file.attributes.dir_id
-            } of io.cozy.files ${file.id || file._id}`
-          )
-          // nothing to do here
-          // @TODO send a sentry event
-        }
-      }
-      getParent(file)
-    },
-    [file.attributes.dir_id]
-  )
+  const file = useFileWithPath({ cozyClient: client, file: props.file })
+  const noteId = file && file.id
 
   const [showModal, setShowModal] = useState(false)
   const onClick = useCallback(() => setShowModal(!showModal), [])
   const onClose = useCallback(() => setShowModal(false), [])
-  const noteId = file.id
+
   return (
-    (parent && (
+    (file && (
       <LocalizedSharingProvider doctype={file.type} documentType="Notes">
         <ShareButton
           theme="primary"
           docId={noteId}
           onClick={onClick}
-          label=""
           extension="narrow"
-          iconOnly
+          className={styles['sharing-button']}
         />
         {showModal && <ShareModal document={file} onClose={onClose} />}
       </LocalizedSharingProvider>
---
+++ src/hooks/useFileWithPath.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import { models } from 'cozy-client'
+
+function useFileWithPath({ cozyClient, file }) {
+  const [fileWithPath, setFileWithPath] = useState(undefined)
+
+  useEffect(
+    () => {
+      async function getParent(rawFile) {
+        const file = models.file.normalize(rawFile)
+        try {
+          const parent = await cozyClient.query(
+            cozyClient.get('io.cozy.files', file.attributes.dir_id)
+          )
+          setFileWithPath(models.file.ensureFilePath(file, parent.data))
+        } catch (e) {
+          // eslint-disable-next-line no-console
+          console.warn(
+            `Request failed when try to fetch the parent ${
+              file.attributes.dir_id
+            } of io.cozy.files ${file.id}`
+          )
+          setFileWithPath(null)
+          // nothing to do here
+          // @TODO send a sentry event
+        }
+      }
+
+      getParent(file)
+    },
+    [file.attributes.dir_id]
+  )
+
+  if (fileWithPath && fileWithPath.id == file.id) {
+    return fileWithPath
+  } else {
+    return undefined
+  }
+}
+
+export default useFileWithPath
