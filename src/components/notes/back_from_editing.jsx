63fc4ec0ba8d9a372fd0357156d3875fc5fc14e2
Éric D <15271+edas@users.noreply.github.com>
2020-01-23 16:47:59 +0100

    feat: Confirmation on exit when changing page
    Following the previous change, this adds a user generated
    modal asking for confirmation when the user try to change
    the view and there is still something some modifications
    not saved with the server

--- src/components/notes/back_from_editing.jsx
+++ src/components/notes/back_from_editing.jsx
@@ -1,12 +1,53 @@
 import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
 import { Button, ButtonLink } from 'cozy-ui/react/Button'
 import IsPublicContext from 'components/IsPublicContext'
 import AppLinker from 'cozy-ui/react/AppLinker'
 import { getFolderLink, getParentFolderId } from 'lib/utils'
 
-export default function BackFromEditing({ returnUrl, file }) {
+/**
+ * Simple fake event to detect if the handler
+ * tries to prevent the default action
+ */
+class FakeEvent {
+  preventDefault() {
+    this.prevented = true
+  }
+}
+
+/**
+ * Create a click handler for a link or button
+ *
+ * Always call requestToLeave, with a function
+ * that should mimic the original behaviour (onClick +  href)
+ * @param {function} requestToLeave - as  in useConfirmExit in cozy-ui
+ * @param {string} href - URL to go to
+ * @param {function} onClick -  regular onClick handler for the button or link
+ */
+function createOnClick(requestToLeave, href, onClick) {
+  const go = () => {
+    const ev = new FakeEvent()
+    if (onClick) onClick(ev)
+    if (!ev.prevented) document.location = href
+  }
+  return function(ev) {
+    ev.preventDefault()
+    requestToLeave(go)
+  }
+}
+
+/**
+ * React component, draws a button to go back, outside of the editor
+ *
+ * Default is going back to the root of the app if in a private view
+ * or do nothing if in a public view
+ * @param {string|null} returnUrl - URL to go back to
+ * @param {object|null} file - io.cozy.file object to generate a folder link
+ * @param {function|null} requestToLeave - function, if present, it should
+ * wrap any regular action that should have taken place when clicking the button
+ */
+export default function BackFromEditing({ returnUrl, file, requestToLeave }) {
   const isPublic = useContext(IsPublicContext)
+
   if (returnUrl) {
     const nativePath = getFolderLink(getParentFolderId(file))
     return (
@@ -15,7 +56,7 @@ export default function BackFromEditing({ returnUrl, file }) {
           return (
             <ButtonLink
               icon="previous"
-              onClick={onClick}
+              onClick={createOnClick(requestToLeave, href, onClick)}
               href={href}
               className="sto-app-back"
               subtle
@@ -25,11 +66,12 @@ export default function BackFromEditing({ returnUrl, file }) {
       </AppLinker>
     )
   } else if (!isPublic) {
+    const href = '#/'
     return (
       <Button
         icon="previous"
-        tag={Link}
-        to="/"
+        href={href}
+        onClick={createOnClick(requestToLeave, href)}
         className="sto-app-back"
         subtle
       />
