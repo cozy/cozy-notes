35383af76c055aba20b2ae98058af4d79c0d058a
Éric D <15271+edas@users.noreply.github.com>
2020-02-12 16:46:31 +0100

    fix: Go back to previous folderId

---
+++ src/components/notes/back_from_editing.spec.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { I18n } from 'cozy-ui/react/I18n'
+import { mount } from 'enzyme'
+import en from '../../locales/en.json'
+import BackFromEditing from './back_from_editing'
+
+describe('BackFromEditing', () => {
+  it('mounts', () => {
+    const returnUrl = '/'
+    const requestToLeave = jest.fn()
+    const file = {
+      _id: 'uid',
+      _type: 'io.cozy.files',
+      attributes: {
+        dir_id: 'parentuid'
+      }
+    }
+    const component = mount(
+      <BackFromEditing
+        requestToLeave={requestToLeave}
+        returnUrl={returnUrl}
+        file={file}
+      />,
+      {
+        wrappingComponent: I18n,
+        wrappingComponentProps: {
+          lang: 'en',
+          dictRequire: () => en
+        }
+      }
+    )
+    expect(component.html()).toMatchSnapshot()
+  })
+})
