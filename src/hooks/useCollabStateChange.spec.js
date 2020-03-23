f702281bbc15f7dce0bacd8d45aeb39ef6ad7219
Éric D <15271+edas@users.noreply.github.com>
2020-03-23 15:51:11 +0100

    fix: Saving indicator
    We add errors in the saving indicator messages, this changes fixes these.
    I also tried to refactor the code, which became too complicated.

---
+++ src/hooks/useCollabStateChange.spec.js
@@ -0,0 +1,35 @@
+import useCollabStateChange from './useCollabStateChange'
+import { renderHook, act } from '@testing-library/react-hooks'
+
+function createProvider() {
+  const listeners = []
+  const on = jest.fn().mockImplementation((name, fn) => listeners.push(fn))
+  const off = jest.fn()
+  return { listeners, on, off }
+}
+
+describe('useCollabStateChange', () => {
+  describe('when state changes', () => {
+    it('rerenders', async () => {
+      const collabProvider = createProvider()
+      const { waitForNextUpdate } = renderHook(() =>
+        useCollabStateChange(collabProvider)
+      )
+      expect(collabProvider.on).toHaveBeenCalled()
+      const update = waitForNextUpdate()
+      act(() => {
+        collabProvider.listeners[0]()
+      })
+      await update
+      // should not time out
+    })
+  })
+  describe('after unmount', () => {
+    it('removes the listener', async () => {
+      const collabProvider = createProvider()
+      const { unmount } = renderHook(() => useCollabStateChange(collabProvider))
+      await unmount()
+      expect(collabProvider.off).toHaveBeenCalled()
+    })
+  })
+})
