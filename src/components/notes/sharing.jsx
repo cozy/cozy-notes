265cd6a06d3e66aeebd9a88f9b7b7007c0fe4e64
Yannick Lohse <contact@yannick-lohse.fr>
2020-05-26 10:57:54 +0200

    feat: Upgrade sharing
    Allows to choose the permissions on a shared note

--- src/components/notes/sharing.jsx
+++ src/components/notes/sharing.jsx
@@ -1,35 +1,33 @@
 import React, { useCallback, useState } from 'react'
-import { ShareButton, ShareModal, withLocales } from 'cozy-sharing'
+import { ShareButton, ShareModal } from 'cozy-sharing'
 import { withClient } from 'cozy-client'
 
 import useFileWithPath from 'hooks/useFileWithPath'
 import styles from 'components/notes/sharing.styl'
 
-export default withLocales(
-  withClient(function SharingWidget(props) {
-    const { client } = props
+export default withClient(function SharingWidget(props) {
+  const { client } = props
 
-    const file = useFileWithPath({ cozyClient: client, file: props.file })
-    const noteId = file && file.id
+  const file = useFileWithPath({ cozyClient: client, file: props.file })
+  const noteId = file && file.id
 
-    const [showModal, setShowModal] = useState(false)
-    const onClick = useCallback(() => setShowModal(!showModal), [showModal])
-    const onClose = useCallback(() => setShowModal(false), [])
+  const [showModal, setShowModal] = useState(false)
+  const onClick = useCallback(() => setShowModal(!showModal), [showModal])
+  const onClose = useCallback(() => setShowModal(false), [])
 
-    return (
-      (file && (
-        <>
-          <ShareButton
-            theme="primary"
-            docId={noteId}
-            onClick={onClick}
-            extension="narrow"
-            className={styles['sharing-button']}
-          />
-          {showModal && <ShareModal document={file} onClose={onClose} />}
-        </>
-      )) ||
-      null
-    )
-  })
-)
+  return (
+    (file && (
+      <>
+        <ShareButton
+          theme="primary"
+          docId={noteId}
+          onClick={onClick}
+          extension="narrow"
+          className={styles['sharing-button']}
+        />
+        {showModal && <ShareModal document={file} onClose={onClose} />}
+      </>
+    )) ||
+    null
+  )
+})
