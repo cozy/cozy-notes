0b030ee91fd3e01ebf8ab9a3cea9a6414ffbfdc7
Yannick Lohse <contact@yannick-lohse.fr>
2020-06-04 09:37:23 +0200

    fix: Use tranlated string for read-only mode

--- src/components/notes/EditorCorner.jsx
+++ src/components/notes/EditorCorner.jsx
@@ -3,18 +3,20 @@ import PropTypes from 'prop-types'
 
 import Icon from 'cozy-ui/react/Icon'
 import Tooltip from 'cozy-ui/react/Tooltip'
+import { useI18n } from 'cozy-ui/react/I18n'
 
 import SharingWidget from 'components/notes/sharing'
 
 const EditorCorner = ({ doc, isPublic, isReadOnly }) => {
   if (!isPublic) return <SharingWidget file={doc.file} />
-  else if (isReadOnly)
+  else if (isReadOnly) {
+    const { t } = useI18n()
     return (
-      <Tooltip title={'This note is in read-only mode.'}>
+      <Tooltip title={t('Notes.Editor.read_only')}>
         <Icon icon="lock" color="var(--primaryTextColor)" />
       </Tooltip>
     )
-  else return false
+  } else return false
 }
 
 EditorCorner.propTypes = {
