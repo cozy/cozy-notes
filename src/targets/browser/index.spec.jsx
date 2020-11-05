80db315f3db66c3e2939fac0afc05bb972d07696
Quentin Valmori <quentin.valmori@gmail.com>
2020-11-05 10:10:32 +0100

    feat: Use module.hot.accept instead of react-hot-loader / upgrade CS

--- src/targets/browser/index.spec.jsx
+++ src/targets/browser/index.spec.jsx
@@ -1,5 +1,4 @@
 import { initApp } from './index'
-import { render } from 'react-dom'
 import { getDataset } from 'lib/initFromDom'
 
 jest.mock('lib/initFromDom', () => ({
@@ -19,13 +18,13 @@ describe('app init', () => {
 
   it('loads with the users locale', () => {
     getDataset.mockReturnValue({ cozyLocale: 'fr' })
-    initApp()
-    expect(render.mock.calls[0][0]).toMatchObject({ props: { lang: 'fr' } })
+    const { appLocale } = initApp()
+    expect(appLocale).toEqual('fr')
   })
 
   it('falls back to a supported locale', () => {
     getDataset.mockReturnValue({ cozyLocale: 'made-up-locale' })
-    initApp()
-    expect(render.mock.calls[0][0]).toMatchObject({ props: { lang: 'en' } })
+    const { appLocale } = initApp()
+    expect(appLocale).toEqual('en')
   })
 })
