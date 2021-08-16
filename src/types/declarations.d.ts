20ccd674b3486d32435641130569c7c0fa76503d
Antonin Cezard <acezard@gmail.com>
2021-08-16 10:23:33 +0200

    feat: fix toaster duration

--- src/types/declarations.d.ts
+++ src/types/declarations.d.ts
@@ -1,3 +1,10 @@
 declare module 'cozy-ui/transpiled/react/Alerter' {
-  function error(error: string): void
+  function error(
+    error: string,
+    options?: {
+      duration?: number
+      buttonText?: string
+      buttonAction?: (...args: unknown[]) => void
+    }
+  ): void
 }
