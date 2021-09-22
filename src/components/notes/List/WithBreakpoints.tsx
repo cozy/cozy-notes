9233bf54bedcaa1c54f5c0417f18e6294380cdb6
Antonin Cezard <acezard@gmail.com>
2021-09-22 08:53:23 +0200

    feat: Improve list display on mobile

---
+++ src/components/notes/List/WithBreakpoints.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
+
+import { Breakpoints } from 'types/enums'
+
+interface Props {
+  hideOn?: Breakpoints
+  showOn?: Breakpoints
+}
+
+export const WithBreakpoints: React.FC<Props> = ({
+  hideOn,
+  showOn,
+  children
+}) => {
+  const breakpoints = useBreakpoints()
+  const hidden = hideOn && breakpoints[hideOn]
+  const shown = showOn && breakpoints[showOn]
+  const Component = <>{children}</>
+
+  return hideOn
+    ? hidden
+      ? null
+      : Component
+    : showOn
+    ? shown
+      ? Component
+      : null
+    : Component
+}
