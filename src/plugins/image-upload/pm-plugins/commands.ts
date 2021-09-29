2b8cd9d41cd8c193e13538effb1d11849cc18e95
Antonin Cezard <acezard@gmail.com>
2021-09-29 14:42:36 +0200

    feat: Add new image-upload plugin

---
+++ src/plugins/image-upload/pm-plugins/commands.ts
@@ -0,0 +1,44 @@
+import { InsertedImageProperties } from '@atlaskit/editor-common/provider-factory'
+import { safeInsert } from 'prosemirror-utils'
+import { createExternalMediaNode } from '../utils'
+import { Command } from '@atlaskit/editor-core/types'
+import { ImageUploadPluginState } from '../types'
+import { startUpload } from './actions'
+import { stateKey } from './plugin-key'
+
+export const insertExternalImage: (
+  options: InsertedImageProperties
+) => Command = options => (state, dispatch) => {
+  console.log(state.selection.$to.pos)
+  const pluginState: ImageUploadPluginState = stateKey.getState(state)
+  if (!pluginState.enabled || !options.src) {
+    return false
+  }
+
+  const mediaNode = createExternalMediaNode(options.src, state.schema)
+  if (!mediaNode) {
+    return false
+  }
+
+  if (dispatch) {
+    dispatch(
+      safeInsert(mediaNode, state.selection.$to.pos)(state.tr).scrollIntoView()
+    )
+  }
+  return true
+}
+
+export const startImageUpload: (event?: Event) => Command = event => (
+  state,
+  dispatch
+) => {
+  const pluginState: ImageUploadPluginState = stateKey.getState(state)
+  if (!pluginState.enabled) {
+    return false
+  }
+
+  if (dispatch) {
+    dispatch(startUpload(event)(state.tr))
+  }
+  return true
+}
