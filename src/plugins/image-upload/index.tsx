afb0c6ed1d60328117e212cc21e3492c076949fc
Antonin Cezard <acezard@gmail.com>
2021-09-29 14:42:36 +0200

    feat: Add new image-upload plugin

---
+++ src/plugins/image-upload/index.tsx
@@ -0,0 +1,22 @@
+import { EditorPlugin } from '@atlaskit/editor-core/types'
+import { createPlugin } from './pm-plugins/main'
+import inputRulePlugin from './pm-plugins/input-rule'
+
+const imageUpload = (): EditorPlugin => ({
+  name: 'imageUpload',
+
+  pmPlugins() {
+    return [
+      {
+        name: 'imageUpload',
+        plugin: createPlugin
+      },
+      {
+        name: 'imageUploadInputRule',
+        plugin: ({ schema }) => inputRulePlugin(schema)
+      }
+    ]
+  }
+})
+
+export default imageUpload
