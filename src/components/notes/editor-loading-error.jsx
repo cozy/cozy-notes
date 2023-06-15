caaac0eb598ae1fcc5011de60f61614f5d3de21e
Crash-- <quentin.valmori@gmail.com>
2023-06-15 08:32:28 +0200

    fix: Escape returnURL parameter

--- src/components/notes/editor-loading-error.jsx
+++ src/components/notes/editor-loading-error.jsx
@@ -1,26 +1,28 @@
 import React from 'react'
 import { Empty } from 'cozy-ui/transpiled/react'
-import { translate } from 'cozy-ui/transpiled/react/I18n'
+import Button from 'cozy-ui/transpiled/react/Buttons'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 
 function EditorLoadingError(props) {
-  const { t, returnUrl } = props
-
+  const { returnUrl } = props
+  const { t } = useI18n()
   return (
     <Empty
       icon="cross-small"
       title={t(`Error.loading_error_title`)}
       text={
-        <p
-          className="u-mb-half"
-          dangerouslySetInnerHTML={{
-            __html: returnUrl
-              ? t(`Error.loading_error_text_returnUrl`, { url: returnUrl })
-              : t(`Error.loading_error_text_noReturnUrl`)
-          }}
-        />
+        <>
+          <p className="u-mb-half">
+            {t(`Error.loading_error_text_noReturnUrl`)}
+          </p>
+
+          {returnUrl && (
+            <Button variant="text" label={t('Error.back')} href={returnUrl} />
+          )}
+        </>
       }
     />
   )
 }
 
-export default translate()(EditorLoadingError)
+export default EditorLoadingError
