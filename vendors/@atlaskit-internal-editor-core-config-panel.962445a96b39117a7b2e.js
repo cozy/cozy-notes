2816222ed05d751a5ccc429d59da7d54283b540e
Travis CI User <travis@example.org>
2023-06-29 08:47:13 +0000

    publish: fix: restore '@atlaskit/calendar'  in webpack
    generated from commit 2581a55ecb361457bd08bb6b011264b5e735ab30

---
+++ vendors/@atlaskit-internal-editor-core-config-panel.962445a96b39117a7b2e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2,3],{YOMN:function(n,w,o){}}]);
---
+++ vendors/@atlaskit-internal_media-card.81b58464d5cf02f61765.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3,2],{YOMN:function(n,w,o){}}]);
