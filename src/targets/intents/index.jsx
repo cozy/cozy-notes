ac2c5a8f224be4ca72f9b351b8bd270bdef7057d
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-07-31 10:23:27 +0200

    feat: Add missing providers WebviewIntentProvider and StylesProvider
    copied from targets/browser

--- src/targets/intents/index.jsx
+++ src/targets/intents/index.jsx
@@ -4,7 +4,12 @@ import 'styles/index.css'
 
 import React from 'react'
 import { render } from 'react-dom'
+import {
+  StylesProvider,
+  createGenerateClassName
+} from '@material-ui/core/styles'
 
+import { WebviewIntentProvider } from 'cozy-intent'
 import Intents from 'cozy-interapp'
 import CozyClient, { CozyProvider } from 'cozy-client'
 import flag from 'cozy-flags'
@@ -16,6 +21,17 @@ const manifest = require('../../../manifest.webapp')
 import { getDataset, getDataOrDefault } from 'lib/initFromDom'
 import IntentEditorView from 'components/intents/IntentEditorView'
 import IntentProvider from 'components/intents/IntentProvider'
+
+/*
+With MUI V4, it is possible to generate deterministic class names.
+In the case of multiple react roots, it is necessary to disable this
+feature. Since we have the cozy-bar root, we need to disable the
+feature.
+https://material-ui.com/styles/api/#stylesprovider
+*/
+const generateClassName = createGenerateClassName({
+  disableGlobal: true
+})
 
 document.addEventListener('DOMContentLoaded', () => {
   const data = getDataset()
@@ -39,18 +55,22 @@ document.addEventListener('DOMContentLoaded', () => {
   const intentId = new URLSearchParams(window.location.search).get('intent')
 
   render(
-    <I18n
-      lang={data.locale}
-      dictRequire={lang => require(`../../locales/${lang}`)}
-    >
-      <CozyProvider client={client}>
-        <IntentProvider intentId={intentId}>
-          <MuiCozyTheme>
-            <IntentEditorView />
-          </MuiCozyTheme>
-        </IntentProvider>
-      </CozyProvider>
-    </I18n>,
+    <WebviewIntentProvider>
+      <I18n
+        lang={data.locale}
+        dictRequire={lang => require(`../../locales/${lang}`)}
+      >
+        <StylesProvider generateClassName={generateClassName}>
+          <CozyProvider client={client}>
+            <IntentProvider intentId={intentId}>
+              <MuiCozyTheme>
+                <IntentEditorView />
+              </MuiCozyTheme>
+            </IntentProvider>
+          </CozyProvider>
+        </StylesProvider>
+      </I18n>
+    </WebviewIntentProvider>,
     document.querySelector('[role=application]')
   )
 })
