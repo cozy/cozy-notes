3a21fe4a56b9cc33649c6f37e618f365bbeafa84
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-05-06 11:40:48 +0200

    feat: Replace empty illustration and text

--- src/components/notes/List/EmptyComponent.jsx
+++ src/components/notes/List/EmptyComponent.jsx
@@ -1,6 +1,5 @@
 import { Slugs } from 'constants/strings'
 
-import icon from 'assets/icons/icon_note_empty.svg'
 import Add from 'components/notes/add'
 import useReferencedFolderForNote from 'hooks/useReferencedFolderForNote'
 import React from 'react'
@@ -8,6 +7,7 @@ import React from 'react'
 import { withClient } from 'cozy-client'
 import AppLinker from 'cozy-ui/transpiled/react/AppLinker'
 import Empty from 'cozy-ui/transpiled/react/Empty'
+import NotesIcon from 'cozy-ui/transpiled/react/Icons/Notes'
 import { translate } from 'cozy-ui/transpiled/react/providers/I18n'
 
 const EmptyComponent = ({ t, client }) => {
@@ -16,7 +16,7 @@ const EmptyComponent = ({ t, client }) => {
     <div className="empty">
       <Empty
         id="empty"
-        icon={icon}
+        icon={NotesIcon}
         title={t('Notes.Empty.welcome')}
         text={
           <AppLinker href={notesFolder} app={{ slug: Slugs.Drive }}>
@@ -25,7 +25,7 @@ const EmptyComponent = ({ t, client }) => {
                 <span className="u-mb-half">
                   {t('Notes.Empty.after_created')}{' '}
                   <a href={href} onClick={onClick}>
-                    Cozy Drive
+                    Twake Drive
                   </a>
                   .
                 </span>
